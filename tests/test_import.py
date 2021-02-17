e12027e0f8f59993beb449d4d8566661ff2f812e
smallbomb <rockon590+git@gmail.com>
2021-02-17 18:56:17 +0800

    add user_command testing

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,12 +5,13 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 from data import uData
-from window import square
+from main import user_command
 
 
 def test_data_format():
     assert type(uData.setting) == dict
 
-
-def test_square():
-    square()
+    
+def test_user_command():
+    user_command('t')
+    user_command('x')
