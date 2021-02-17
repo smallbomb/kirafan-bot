8a92f81198ea60d118270d0f9c8250ee115e68e3
smallbomb <rockon590+git@gmail.com>
2021-02-17 18:37:30 +0800

    Update install-dependencies.sh

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -9,4 +9,4 @@ echo "GITHUB_ACTIONS=$GITHUB_ACTIONS"
 echo "VIRTUAL_ENV=$VIRTUAL_ENV"
 
 python -m pip install --disable-pip-version-check --upgrade pip setuptools
-python -m pip install --upgrade -r requirements.txt
+python install.py
