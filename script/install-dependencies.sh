b2203314dd012db1dc13739734c14887f1b2e802
smallbomb <rockon590+git@gmail.com>
2021-02-17 16:37:16 +0800

    Update install-dependencies.sh

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -10,4 +10,3 @@ echo "VIRTUAL_ENV=$VIRTUAL_ENV"
 
 python -m pip install --disable-pip-version-check --upgrade pip setuptools
 python -m pip install --upgrade -r requirements.txt
-python -m pip install -e .
