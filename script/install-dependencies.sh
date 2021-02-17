f1857684a140c564efecc4af025ac0d6b64ac16e
smallbomb <rockon590+git@gmail.com>
2021-02-17 18:49:42 +0800

    Update install-dependencies.sh

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -8,5 +8,7 @@ echo "CI=$CI"
 echo "GITHUB_ACTIONS=$GITHUB_ACTIONS"
 echo "VIRTUAL_ENV=$VIRTUAL_ENV"
 
-python -m pip install --disable-pip-version-check --upgrade pip setuptools
+
+python -m pip install --upgrade pip
+python -m pip install flake8 pytest
 python install.py
