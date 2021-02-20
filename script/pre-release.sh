c2797ca72826f4f2f20265fbf047f19b8d4d4ed7
smallbomb <rockon590+git@gmail.com>
2021-02-20 10:56:48 +0800

    fix automatic-release

--- script/pre-release.sh
+++ script/pre-release.sh
@@ -6,3 +6,6 @@ set -ex
 
 python -m pip install pyinstaller
 pyinstaller -F main.py
+ls
+mv ./dist/main.exe ./kirafan-bot.exe
+tar cvf kirafan-bot-pre-release-win-x64-exe.tar kirafan-bot.exe img_1274x718 setting.json
