a3fdecbf78f118b9b5949f833b40675f792b21be
smallbomb <rockon590+git@gmail.com>
2021-09-28 11:39:42 +0800

    Update build_install.sh
    add LICENSE file

--- script/build_install.sh
+++ script/build_install.sh
@@ -7,4 +7,4 @@ set -ex
 python -m pip install pyinstaller
 pyinstaller -F src/main.py
 mv ./dist/main* ./kirafan-bot.exe
-tar cvf kirafan-bot.tar kirafan-bot.exe bot_setting.json img_1274x718
+tar cvf kirafan-bot.tar kirafan-bot.exe bot_setting.json img_1274x718 LICENSE
