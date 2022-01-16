9ba38dc4fff62eaea6a17a313a178c5d108de367
smallbomb <rockon590+git@gmail.com>
2022-01-16 10:09:41 +0800

    increase the first processing speed in visiting friend room

--- src/visit_friend_room.py
+++ src/visit_friend_room.py
@@ -14,8 +14,7 @@ def run(window):
         if tries == 3:
             bot.stop()
 
-        _handle_friend_icon(bot, tries)
-        if _handle_visit_room(bot, tries):
+        if _handle_visit_room(bot, tries, _handle_friend_icon(tries)):
             tries += 1
             logging.info(f'visit_friend time = {tries} success')
             sleep(kirafan.sleep['loading'])
@@ -26,28 +25,27 @@ def run(window):
 
 
 @typechecked
-def _handle_friend_icon(bot, tries: int):
+def _handle_friend_icon(tries: int) -> bool:
     sec = 2 if tries == 0 else 5
     friend_icon_retry = True
-    while bot.is_running():
+    while True:
         if kirafan.icons['friend_icon'].scan(sec):
             kirafan.icons['friend_icon'].click(2, adb_update_cache=False)
             sleep(2)
-            break
-        elif friend_icon_retry:
+            return True
+        elif tries != 0 and friend_icon_retry:
             logging.debug('try a again because friend_icon.png not found')
             friend_icon_retry = False
         else:
-            break
+            return False
 
 
 @typechecked
-def _handle_visit_room(bot, tries: int) -> bool:
-    sec = 2 if tries == 0 else 5
-    visit_room_retry = True
+def _handle_visit_room(bot, tries: int, found_friend_icon: bool) -> bool:
+    sec, visit_room_retry = (2, False) if tries == 0 and not found_friend_icon else (5, True)
     while bot.is_running():
         if kirafan.icons['visit_room'].scan(sec):
-            kirafan.icons['visit_room'].click(adb_update_cache=False)
+            kirafan.icons['visit_room'].click(2, adb_update_cache=False)
             return True
         elif kirafan.icons['tojiru'].click(adb_update_cache=False):
             logging.error('No friend room....')
@@ -55,7 +53,10 @@ def _handle_visit_room(bot, tries: int) -> bool:
         elif visit_room_retry:
             logging.debug('try a again because visit_room.png not found')
             visit_room_retry = False
-        else:
+        elif tries == 0:
             logging.error('please move to room!')
+            bot.stop()
+        else:
+            logging.error('unknown error...')
             bot.stop()
     return False
