36d6b6210cf0f79fb4832cb95c225fe059b28783
smallbomb <rockon590+git@gmail.com>
2022-01-25 18:03:14 +0800

    add test function

--- tests/test_import.py
+++ tests/test_import.py
@@ -6,15 +6,26 @@ if True:
     sys.path.insert(0, parent_dir)
     from data import uData
     from hotkey import Hotkey
-    from main import check_basic_information
 
 
 def test_data_format():
     assert type(uData.setting) == dict
+    assert type(uData.gui_setting()) == dict
+    assert uData.save_location(0, 31) is None
+    assert uData.gui_setting()['location'] == [0, 31]
 
 
-def test_hotkey_and_mainfunc():
-    if os.name == 'nt':
-        hotkey = Hotkey('rslmptcoxk')
-        assert hotkey.safe_exit() is None
-        assert check_basic_information() is None
+def test_hotkey():
+    hotkey = Hotkey('rslmptcoxk')
+    assert hotkey.safe_exit() is None
+    assert hotkey._Hotkey__user_command('1') is None
+    assert hotkey._Hotkey__user_command('r') is None
+    assert hotkey._Hotkey__user_command('s') is None
+    assert hotkey._Hotkey__user_command('l') is None
+    # assert hotkey._Hotkey__user_command('m') is None
+    assert hotkey._Hotkey__user_command('t') is None
+    assert hotkey._Hotkey__user_command('c') is None
+    assert hotkey._Hotkey__user_command('o') is None
+    # assert hotkey._Hotkey__user_command('x') is None
+    assert hotkey._Hotkey__user_command('k') is None
+    assert hotkey.remove_all_hotkey() is None
