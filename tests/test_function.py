3ae306392b61beea1a9c344548e06e50529fdcbe
smallbomb <rockon590+git@gmail.com>
2022-01-29 09:14:27 +0800

    fix test function.py with screenshot

--- tests/test_function.py
+++ tests/test_function.py
@@ -43,10 +43,10 @@ def test_gui():
                '_0_example_wave_total_': 5, '_0_example_wave1_auto_': False, '_0_example_wave1_sp_weight_enable_': True,
                '_0_example_wave1_character_left_sp_weight_': 4, '_0_example_wave1_character_middle_sp_weight_': 6,
                '_0_example_wave1_character_right_sp_weight_': 8, '_timer_use_': True, '_adb_use_': True}
+    kirafanbot_gui.bt_screenshot_event()
     for k in testing.keys():
         kirafanbot_gui.handle_tab_event(tab, k, testing)
         kirafanbot_gui.handle_adb_event(k, testing[k])
-    kirafanbot_gui.bt_screenshot_event()
     uData.reload()
 
     assert kirafanbot_gui.find_tab_by_key('_0_example_orb1_use_') is not None
