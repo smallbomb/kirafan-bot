44ed5c402b3c9604a454a1607d42945a248b3c97
smallbomb <rockon590+git@gmail.com>
2022-01-30 01:27:17 +0800

    revert c1a7e3

--- scripts/build_install.sh
+++ scripts/build_install.sh
@@ -5,7 +5,6 @@
 set -ex
 
 python -m pip install pyinstaller
-pyinstaller -F src/main.py --noconsole
-[[ -f ./dist/main ]] && file=./dist/main || file=./dist/main.exe
-mv "$file" ./kirafan-bot.exe
+pyinstaller -F src/main.py
+mv ./dist/main* ./kirafan-bot.exe
 tar cvf kirafan-bot.tar kirafan-bot.exe bot_setting.json advanced_setting.jsonc img_1280x720 LICENSE
